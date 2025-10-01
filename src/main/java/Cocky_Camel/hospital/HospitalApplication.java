5af5af272fc5234e16f3ad0f9b30db59bd08ffa8
Eric Rodriguez Rojo <erpys02@gmail.com>
2025-10-01 16:50:57 +0200

    Hemos resuelto un par de problemas y ya esta finalizado para usarlo en comun #4

--- src/main/java/Cocky_Camel/hospital/HospitalApplication.java
+++ src/main/java/Cocky_Camel/hospital/HospitalApplication.java
@@ -2,21 +2,31 @@ package Cocky_Camel.hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class HospitalApplication {
-
-	List<Nurse> nurses = new ArrayList<>();
+	public static List<Nurse> nurses = new ArrayList<>();
 
 	public static void main(String[] args) {
-		nurses.add(new Nurse("Dolores Curitas"));
-		nurses.add(new Nurse("Ana Sanguina"));
-		nurses.add(new Nurse("Clara U. Estresada"));
-		nurses.add(new Nurse("Enfermera McStitch"));
-		nurses.add(new Nurse("Beti Bandaid"));
-		nurses.add(new Nurse("Inyecciones Joy"));
-		nurses.add(new Nurse("Marga Rina"));
-		nurses.add(new Nurse("Salud P. Rápida"));
+		SpringApplication.run(HospitalApplication.class, args);
+		Nurse nurse1 = new Nurse("Pepe", "rodri", "123");
+		Nurse nurse2 = new Nurse("Mario", "lopez", "333");
+		Nurse nurse3 = new Nurse("Felipe", "cast", "321");
+
+		nurses.add(nurse1);
+		nurses.add(nurse2);
+		nurses.add(nurse3);
+
 	}
 
+	@GetMapping("/nurse/prueba/{prueba}")
+	public String nurse(@PathVariable String prueba) {
+		return String.format("Hello %s!", prueba);
+	}
 }
---
+++ src/main/java/Cocky_Camel/hospital/Nurse.java
@@ -0,0 +1,14 @@
+package Cocky_Camel.hospital;
+
+public class Nurse {
+	private String name;
+	private String user;
+	private String password;
+
+	public Nurse(String name, String user, String password) {
+		this.name = name;
+		this.user = user;
+		this.password = password;
+	}
+
+}
