4da8f142a8e7fbd4eede3d3781cbd36bcd9eb462
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2025-10-01 16:11:58 +0200

    Commit de el array list de nurses para que sea comun para todos #3

--- src/main/java/Cocky_Camel/hospital/HospitalApplication.java
+++ src/main/java/Cocky_Camel/hospital/HospitalApplication.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HospitalApplication {
 
+	List<Nurse> nurses = new ArrayList<>();
+
 	public static void main(String[] args) {
-	
+		nurses.add(new Nurse("Dolores Curitas"));
+		nurses.add(new Nurse("Ana Sanguina"));
+		nurses.add(new Nurse("Clara U. Estresada"));
+		nurses.add(new Nurse("Enfermera McStitch"));
+		nurses.add(new Nurse("Beti Bandaid"));
+		nurses.add(new Nurse("Inyecciones Joy"));
+		nurses.add(new Nurse("Marga Rina"));
+		nurses.add(new Nurse("Salud P. Rápida"));
 	}
 
 }
