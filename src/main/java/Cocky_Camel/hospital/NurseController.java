1553384a4026db2a1aa0e48a75e6e58ef1b4d220
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2025-10-03 15:45:54 +0200

    Búsqueda de enfermeros por nombre sin tener en cuenta los JSON (DE MOMENTO) #10

--- src/main/java/Cocky_Camel/hospital/NurseController.java
+++ src/main/java/Cocky_Camel/hospital/NurseController.java
@@ -1,17 +1,34 @@
 package Cocky_Camel.hospital;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NurseController {
-	private List<Nurse> nurses = new ArrayList<>();	
-	
-	public void Nurses() {
-		Nurse nurse1 = new Nurse("Pepe", "rodri", "123");
-		Nurse nurse2 = new Nurse("Mario", "lopez", "333");
-		Nurse nurse3 = new Nurse("Felipe", "cast", "321");
-		nurses.add(nurse1);
-		nurses.add(nurse2);
-		nurses.add(nurse3);
+
+	private List<Nurse> nurses = new ArrayList<>();
+
+	@GetMapping("/nurse/name/{name}")
+	public ResponseEntity<String> findByName(@PathVariable String name) {
+		nurses.clear();
+		nurses.add(new Nurse("Pepe", "rodri", "123"));
+		nurses.add(new Nurse("Mario", "lopez", "333"));
+		nurses.add(new Nurse("Felipe", "cast", "321"));
+
+		for (Nurse nurse : nurses) {
+			if (nurse.getName().equalsIgnoreCase(name)) {
+				String response = "Enfermero encontrado: " + nurse.getName() + " | Usuario: " + nurse.getUser()
+						+ " | Password: " + nurse.getPassword();
+				return ResponseEntity.ok(response);
+			}
+		}
+
+		return ResponseEntity.status(404).body("No se encontró ningún enfermero con el nombre: " + name);
 	}
-	
+
 }
