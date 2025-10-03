2e3f2fc101655a815f963afee6578bc270e4a49b
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2025-10-03 15:49:08 +0200

    Unificar codigo con el del Eric #10

--- src/main/java/Cocky_Camel/hospital/Nurse.java
+++ src/main/java/Cocky_Camel/hospital/Nurse.java
@@ -1,9 +1,17 @@
 package Cocky_Camel.hospital;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Nurse {
+	@JsonProperty("Name")
 	private String name;
+	@JsonProperty("User")
 	private String user;
+	@JsonProperty("Password")
 	private String password;
+
+	public Nurse() {
+	}
 
 	public Nurse(String name, String user, String password) {
 		this.name = name;
@@ -11,4 +19,27 @@ public class Nurse {
 		this.password = password;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
